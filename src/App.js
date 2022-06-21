0acbff881f61eb1dc76edaed3c7a05dd10f43167
Augusto Ciurleo <ciurleoa98@gmail.com>
2022-06-20 22:53:34 -0300

    app

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tienda Moonshine Art
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          rel="noopener noreferrer" 
         >
-          Learn React
+          REACT JS - CoderHouse
         </a>
       </header>
     </div>
