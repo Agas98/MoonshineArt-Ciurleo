58f5abefcb82fa3ccc6c9db6e6e5894b70506973
Augusto Ciurleo <ciurleoa98@gmail.com>
2022-06-28 21:20:10 -0300

    agrego navbar

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      M0ONSHINE ART
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Aros
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Colgantes
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Gargantillas
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Anillos
+          </a>
+        </li>
+      
+      </ul>
+    </div>
+  </div>
+</nav>
+  )
+}
